5fcfeb1110cbec7154b68268fd7ee8db54b8a501
anandhus-calixto <anandhu.s@calixto.co.in>
2026-03-12 18:41:57 +0530

    ethernet,otg,usb_host,rs485-checked-working

---
+++ include/generated/compile.h
@@ -0,0 +1,4 @@
+#define UTS_MACHINE		"arm"
+#define LINUX_COMPILE_BY	"calixto"
+#define LINUX_COMPILE_HOST	"calixto-HP-Notebook"
+#define LINUX_COMPILER		"arm-linux-gnueabihf-gcc (Ubuntu 11.4.0-1ubuntu1~22.04) 11.4.0, GNU ld (GNU Binutils for Ubuntu) 2.38"
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "6.12.49-gcce52ae743f4-dirty"
